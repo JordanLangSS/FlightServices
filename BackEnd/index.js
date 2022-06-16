0ca6f580bb83eb49eb282a9000611b078e30e4ca
Jordan Lang <jlang@skillstorm.com>
2022-06-16 17:51:09 -0400

    add controller,model,route

---
+++ BackEnd/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// use a .env file for the server port in case I want a Dev and client port separate
+require('dotenv').config(); // so I don't need the value from the require
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+    })
+    .catch(err => {
+        console.error(err);
+        //terminate process if failed to connect to DB
+        process.exit(1);
+    });
+
+// Will print only after system fails and exits
+process.on('exit', () => {
+    console.log('Cleanup after exit.')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ BackEnd/models/flight.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a flight schema
+const flightSchema = new Schema({
+    flightNumber: {
+        Number,
+        unique: true,
+        required: [true, 'Must provide a flight number'],
+        min: [1, 'Flight number must be greater than 0']
+    },
+    departureDate: {
+        type: String,
+        required: [true, 'Must provide a departure date']
+    },
+    arrivalDate: {
+        type: String,
+        required: [true, 'Must provide an arrival date']
+    },
+    departureTime: {
+        type: String,
+        required: [true, 'Must provide a departure time']
+    },
+    arrivalTime: {
+        type: String,
+        required: [true, 'Must provide an arrival time']
+    },
+    departureAirport: {
+        type: String,
+        required: [true, 'Must provide a departure airport']
+    },
+    arrivalAirport: {
+        type: String,
+        required: [true, 'Must provide an arrival airport']
+    },
+    currentNumOfPassengers: {
+        type: Number,
+        required: [true, 'Must provide a current number of passengers'],
+        min: [0, 'Current number of passengers must be greater than or equal to 0']
+    },
+    passengerLimit: {
+        type: Number,
+        required: [true, 'Must provide a passenger limit'],
+        min: [1, 'Passenger Limit must be greater than 0']
+    }
+
+});
